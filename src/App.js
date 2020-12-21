315d65acb17f51bad6b2eab36ea26fe3786b2ffa
Michael Aigboeghian <micki1.com@gmail.com>
2020-12-21 10:20:29 +0100

    Search box and background implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Searchbox } from "./components/search-box/search-box.component";
 import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
@@ -7,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -16,10 +18,24 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <Searchbox
+          placeholder="Search monsters"
+          handleChange={this.handleChange}
+        />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
