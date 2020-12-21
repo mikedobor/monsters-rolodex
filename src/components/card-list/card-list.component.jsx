74fb6fd39bf5681a605e3a2065d0995afa38b612
Michael Aigboeghian <micki1.com@gmail.com>
2020-12-21 07:12:36 +0100

    Card component implementation

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = (props) => {
-    return (<div className="Card-list">
-        {props.children}
+    return (<div className="card-list">
+        {props.monsters.map((monster) => (
+            <Card key={monster.id} monster={monster} />
+        ))}
     </div>)
 }
